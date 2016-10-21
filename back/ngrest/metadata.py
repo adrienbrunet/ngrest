6544910937dd15ad65d8076e9ccfa8fdf10bf110
Xavier Ordoquy <xordoquy@linovia.com>
2016-10-21 10:15:44 +0200

    Metadata now outputs some templateOptions.

--- back/ngrest/metadata.py
+++ back/ngrest/metadata.py
@@ -34,6 +34,35 @@ class NgMetadata(SimpleMetadata):
         serializers.Serializer: 'nested object',
     })
 
+    template_type_lookup = ClassLookupDict({
+        serializers.Field: 'field',
+        serializers.BooleanField: 'boolean',
+        serializers.NullBooleanField: 'boolean',
+        serializers.CharField: 'string',
+        serializers.URLField: 'url',
+        serializers.EmailField: 'email',
+        serializers.RegexField: 'regex',
+        serializers.SlugField: 'slug',
+        serializers.IntegerField: 'integer',
+        serializers.FloatField: 'float',
+        serializers.DecimalField: 'decimal',
+        serializers.DateField: 'date',
+        serializers.DateTimeField: 'datetime',
+        serializers.TimeField: 'time',
+        serializers.ChoiceField: 'choice',
+        serializers.MultipleChoiceField: 'multiple choice',
+        serializers.FileField: 'file upload',
+        serializers.ImageField: 'image upload',
+        serializers.ListField: 'list',
+        serializers.DictField: 'nested object',
+        serializers.Serializer: 'nested object',
+    })
+
+    option_name_lookup = {
+        'help_text': 'placeholder',
+        'label': 'label',
+    }
+
     def get_serializer_info(self, serializer):
         """
         Given an instance of a serializer, return a dictionary of metadata
@@ -53,20 +82,25 @@ class NgMetadata(SimpleMetadata):
         Given an instance of a serializer field, return a dictionary
         of metadata about it.
         """
-        field_info = OrderedDict([('name', field_name)])
+        field_info = OrderedDict([
+            ('name', field_name),
+            ('templateOptions', {}),
+        ])
         field_info['type'] = self.label_lookup[field]
-        # field_info['required'] = getattr(field, 'required', False)
+        field_info['templateOptions']['required'] = getattr(
+            field, 'required', False)
 
-        attrs = [
-            # 'read_only', 'label', 'help_text',
-            # 'min_length', 'max_length',
-            # 'min_value', 'max_value',
-        ]
+        # attrs = [
+        #     'read_only', 'label', 'help_text',
+        #     'min_length', 'max_length',
+        #     'min_value', 'max_value',
+        # ]
 
-        for attr in attrs:
-            value = getattr(field, attr, None)
+        for src_attr, dest_attr in self.option_name_lookup.items():
+            value = getattr(field, src_attr, None)
             if value is not None and value != '':
-                field_info[attr] = force_text(value, strings_only=True)
+                field_info['templateOptions'][dest_attr] = force_text(
+                    value, strings_only=True)
 
         if getattr(field, 'child', None):
             field_info['child'] = self.get_field_info(field.child)
